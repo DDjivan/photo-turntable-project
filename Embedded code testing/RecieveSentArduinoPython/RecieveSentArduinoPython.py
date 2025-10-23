3770324cc8760b064befd919f4ad2df7864ce25b
δDD <64313188+DDjivan@users.noreply.github.com>
2025-10-23 09:19:07 +0200

    feat: refactor of RecieveSentArduinoPython.py

--- Embedded code testing/RecieveSentArduinoPython/RecieveSentArduinoPython.py
+++ Embedded code testing/RecieveSentArduinoPython/RecieveSentArduinoPython.py
@@ -1,36 +1,63 @@
-import serial
+#!/bin/env python
+
+from serial import Serial
 import sys
-import time
+from time import sleep
 
 
-ser = serial.Serial('COM5', baudrate = 9600, timeout=1)
-time.sleep(2)
 
-def send_Message(pMessage):
-        print("Send: " + pMessage)
-        ser.write((pMessage + '\n').encode('utf-8'))
+def send_Message(ser:Serial, pMessage:str) -> None:
+    print(f"Send: {pMessage}")
+    ser.write((pMessage + '\n').encode('utf-8'))
+    return
 
-def recieve_Message():
-        if ser.in_waiting > 0:
-                incoming_Message = ser.readline().decode('utf-8').strip()
-                print("Recieved: " + incoming_Message)
+def recieve_Message(ser:Serial) -> None:
+    if ser.in_waiting <= 0:
+        return
+    
+    incoming_Message = ser.readline().decode('utf-8').strip()
+    print("Recieved: " + incoming_Message)
 
-                if incoming_Message == "Arduino: Ready!": print("We are ready to communicate")
-                elif incoming_Message == "Pong": print("You can excecute any command")                                     
-                elif incoming_Message == "bla": send_Message("bla")
-                elif incoming_Message == "Bye": print("")
-                elif incoming_Message == "Hello": print("")
-                
-                else: print("recieved unknown Message: " + incoming_Message)
+    match incoming_Message:
+        case "Arduino: Ready!":
+            print("We are ready to communicate")
+        case "Pong":
+            print("You can excecute any command")
+        case "bla":
+            send_Message(ser, "bla")
+        case "Bye":
+            print("")
+        case "Hello":
+            print("")
+        case _:
+            print("recieved unknown Message: " + incoming_Message)
 
-try:
-        send_Message("Ping")
-        while 1:
-                recieve_Message()
-                time.sleep(0.1)
-except KeyboardInterrupt:
-        print("\nProgramm stoped.")
-finally:
-        ser.close()
+    return
+
+
+
+def main() -> None:
+    # on MS Windows:    "COM###"
+    # on Linux:         "/dev/ttyUSB###"
+    port_device = "/dev/ttyUSB0"
+    myser = Serial(port_device, baudrate = 9600, timeout = 1)
+    sleep(2)
+
+    try:
+        send_Message(myser, "Ping")
+        while True:
+            recieve_Message(myser)
+            sleep(0.1)
+
+    except KeyboardInterrupt:
+        print("")
+        print("Programm stoped.")
+
+    finally:
+        myser.close()
         print("Serial closed.")
-	
+
+    return
+
+if __name__ == "__main__":
+    main()
